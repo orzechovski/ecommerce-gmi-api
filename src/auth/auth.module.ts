19abd763070428ca91c354cb06a8630c6d369e69
Marcin Orzechowski <mar.orzechowski.99@gmail.com>
2024-10-04 16:24:20 +0200

    Refactor database configuration in prisma/schema.prisma and add directUrl environment variable

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { DatabaseModule } from 'src/database/database.module';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '24h' },
     }),
   ],
   controllers: [AuthController],
