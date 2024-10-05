ac45d3916944c64d4966b70b5bcaa78661a19ad0
Marcin Orzechowski <mar.orzechowski.99@gmail.com>
2024-10-05 23:54:35 +0200

    delete cors headers string defining

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,6 @@ async function bootstrap() {
     origin: ['http://localhost:3001', 'https://ecommerce-gmi-front.vercel.app'],
     methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
     credentials: true,
-    allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
   });
 
   const swaggerConfig = new DocumentBuilder()
